3ce50db8e9af2b78849e90e65c1a4f873e29af00
adri1274 <a.anayap@upam.edu.mx>
2024-12-13 10:34:45 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+// Configuración de la base de datos
+$host = "localhost"; // Servidor de la base de datos
+$user = "root"; // Usuario de la base de datos
+$password = ""; // Contraseña del usuario
+$dbname = "lingus"; // Nombre de la base de datos
+
+// Crear conexión
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Conexión exitosa
+?>
